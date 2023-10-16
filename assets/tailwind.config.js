aa9613321d4e38ce8bc0792a76146d089eed4311
sp <sparks@localhost.localdomain>
2023-10-16 15:21:16 +0300

    still styling the nav nav bar and some of the auth page

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -14,7 +14,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: "#FD4F00",
+        emDark:{
+          dark: "#010101",
+          DEFAULT: "#3C3B3B",
+          list: "#828282"
+        },
+        emPurple:{
+          dark: "#631A61",
+          DEFAULT: "#A834A5",
+          light: "#F88FE5"
+        },
+        emLavender: {
+          dark:"#8C52FF",
+          DEFAULT:"#9D74EE",
+          light: "#BC94FF"
+        }
+      },
+      fontFamily:{
+        brand:["WorkSans", "sans-serif"]
+      },
+      fontWeight:{
+        regular: "normal",
+        bold: "bold"
+
       }
     },
   },
